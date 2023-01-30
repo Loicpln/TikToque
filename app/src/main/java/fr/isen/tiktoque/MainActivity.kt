9051dc11a298b650704e5fa6291b793985a77bd8
Loicpln <60894541+Loicpln@users.noreply.github.com>
2023-01-30 13:19:29 +0100

    firebase

--- app/src/main/java/fr/isen/tiktoque/MainActivity.kt
+++ app/src/main/java/fr/isen/tiktoque/MainActivity.kt
@@ -1,11 +1,42 @@
 package fr.isen.tiktoque
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import fr.isen.tiktoque.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Firebase.database.getReference("message").addValueEventListener(object :
+            ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+                findViewById<Button>(R.id.button).text = value
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+
+
+        }
+
     }
 }
