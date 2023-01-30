f6ca4033eea09aecac0785e62dac532e513d09f1
Loicpln <60894541+Loicpln@users.noreply.github.com>
2023-01-30 15:41:54 +0100

    user

--- app/src/main/java/fr/isen/tiktoque/MainActivity.kt
+++ app/src/main/java/fr/isen/tiktoque/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.signUpButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
