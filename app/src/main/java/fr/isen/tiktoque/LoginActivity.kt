6c085b3300f23d02e1669d09231da3b43acd6bf5
Loicpln <60894541+Loicpln@users.noreply.github.com>
2023-01-30 16:57:50 +0100

    login

--- app/src/main/java/fr/isen/tiktoque/LoginActivity.kt
+++ app/src/main/java/fr/isen/tiktoque/LoginActivity.kt
@@ -1,11 +1,25 @@
 package fr.isen.tiktoque
 
+import android.content.ClipData
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
 import fr.isen.tiktoque.databinding.ActivityLoginBinding
+import fr.isen.tiktoque.model.User
+import java.io.File
 
 class LoginActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -13,10 +27,34 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*binding.loginButton.setOnClickListener {
-            val intent = Intent(this, FeedActivity::class.java)
-            startActivity(intent)
-        }*/
-
+        binding.submitButton.setOnClickListener{
+            signIn(binding.email.text.toString(), binding.password.text.toString())
+        }
     }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            // L'utilisateur est connecté, affichez les informations de l'utilisateur
+            //TODO
+        }
+    }
+
+
 }
