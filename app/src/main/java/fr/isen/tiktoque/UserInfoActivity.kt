e536c18aa730017859badc947528fdccdaf57075
Ghostrak23 <vincentghost75@gmail.com>
2023-02-03 09:07:07 +0100

    User info

--- app/src/main/java/fr/isen/tiktoque/UserInfoActivity.kt
+++ app/src/main/java/fr/isen/tiktoque/UserInfoActivity.kt
@@ -1,22 +1,39 @@
 package fr.isen.tiktoque
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Button
-import android.widget.ImageView
+import fr.isen.tiktoque.databinding.ActivityUserInfoBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import fr.isen.tiktoque.model.User
+
 
 class UserInfoActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityUserInfoBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var uuidPhoto: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
+        binding = ActivityUserInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.saveUserInfo.setOnClickListener { saveUserInfo() }
+    }
+
+
+    private fun saveUserInfo() {
+        val username = binding.userUsername.text.toString().trim()
+        val age = binding.userAge.text.toString().trim()
+        val phone = binding.userPhone.text.toString().trim()
+
+        val database = FirebaseDatabase.getInstance()
+        val reference = database.getReference("users")
+        val userId = reference.push().key
+        val user = User(userId, username, age, phone)
+        if (userId != null) {
+            reference.child(userId).setValue(user)
+        }
     }
 }
